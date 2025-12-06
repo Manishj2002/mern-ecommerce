c4f61c3058a0796c9160590d61096c40adc077fb
manish jakhar <manish946050@gmail.com>
2025-12-06 12:10:48 +0530

    Fix server start script for Render

--- server/server.js
+++ server/server.js
@@ -28,18 +28,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
     credentials: true,
   })
 );
+
 
 app.use(cookieParser());
 app.use(express.json());
