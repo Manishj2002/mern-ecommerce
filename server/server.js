05f33522a6023d1e840c41e02789b4b924643f5e
manish jakhar <manish946050@gmail.com>
2025-12-06 13:22:26 +0530

    added link in backend

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: 'https://mern-ecommerce-delta-three.vercel.app',
+    origin: 'https://mern-ecommerce-cmgu.vercel.app',
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
